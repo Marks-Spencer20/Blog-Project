be56a241bcb548e4ade69d72b137dbce6fbefd62
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-17 16:01:49 +0000

    something small

--- src/routes/Sports.js
+++ src/routes/Sports.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import SportsNavbar from '../components/SportsNavbar';
 import "./Sports.css"
+import pic1 from '../images/blog2.png'
 
 const Sports = () => {
   return (
@@ -11,4 +12,15 @@ const Sports = () => {
     </div>
   )
 }
+
+<div className='container'>
+  <div className='picture'>
+  <img src={pic1} alt='pic1' className='pic1' width='250px'/> 
+  </div>
+  <div className='heading'>
+    <h1>
+      hello Marksss
+    </h1>
+  </div>
+</div>
 export default Sports;
