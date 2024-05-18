e1bbcf0b2913925580302ff6359dcf161032d13f
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-18 16:13:35 +0000

    adding up

--- src/routes/Football.js
+++ src/routes/Football.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Home.css"
 import Navbar from '../components/Navbar'
 import SportsNavbar from '../components/SportsNavbar'
+import aitana from '../images/aitana.jpg'
 
 const Football = () => {
   return (
@@ -10,6 +11,22 @@ const Football = () => {
     <div >
          <Navbar/>
          <SportsNavbar/>
+
+         <div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h1>
+      hello Markss
+    </h1>
+  </div>
+</div>
+
+
+
+
+
     </div>
   )
 }
--- src/routes/Sports.js
+++ src/routes/Sports.js
@@ -4,6 +4,13 @@ import SportsNavbar from '../components/SportsNavbar';
 import "./Sports.css"
 import aitana from '../images/aitana.jpg'
 import xavi from '../images/xavi.jpg'
+import leverkusen from '../images/leverkusen.jpg'
+import argentina from '../images/argentina.jpg'
+import laporta from '../images/laporta.jpg'
+import thomastuchel from '../images/thomastuchel.jpg'
+
+
+
 
 const Sports = () => {
   return (
@@ -31,44 +38,57 @@ const Sports = () => {
 
 <div className='container'>
   <div className='picture'>
-  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  <img src={leverkusen} alt='pic1' className='pic1' width='200px'/> 
   </div>
   <div className='sub-heading'>
     <h4 className='text2'>
-      hello Markss
+      Bayer 04 Leverkusen is still unbeaten for this season. 
+      The unbeaten run for Xabi's Alonso still continues after 
+      playing 50 games this semester.
     </h4>
   </div>
 </div>
 
 <div className='container'>
   <div className='picture'>
-  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  <img src={argentina} alt='pic1' className='pic1' width='200px'/> 
   </div>
   <div className='sub-heading'>
     <h4 className='text2'>
-      hello Markss
+      The defending champions of the Copa America, Argentina managed 
+      by Lionel Scaloni has dropped his squad for this years edition 
+      of the Copa America. 
     </h4>
   </div>
 </div>
 
 <div className='container'>
   <div className='picture'>
-  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  <img src={laporta} alt='pic1' className='pic1' width='200px'/> 
   </div>
   <div className='sub-heading'>
     <h4 className='text2'>
-      hello Markss
+      Joan Laporta is seriously furious with Xavi Hernadez for talking
+      about the club in a way that he is not supposed to do. 
     </h4>
   </div>
 </div>
 
+<div className='heading1'>
+    <h5 className='text1'>
+      Rumour!
+    </h5>
+  </div>
+
 <div className='container'>
   <div className='picture'>
-  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  <img src={thomastuchel} alt='pic1' className='pic1' width='200px'/> 
   </div>
   <div className='sub-heading'>
     <h4 className='text2'>
-      hello Markss
+      Bayern Munich tried to convince Thomas Tuchel to change his mind 
+      about leaving the club at the end of the season, but unfortunately
+       thinggs didn't go as planned.
     </h4>
   </div>
 </div>
@@ -142,6 +162,7 @@ const Sports = () => {
 
     </div>
   )
+  
 }
 
 export default Sports;
