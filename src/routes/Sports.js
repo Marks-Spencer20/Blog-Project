d315cc4cee2f9c02ae26858cbf9f03bb987e5230
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-18 14:20:25 +0000

    ongoing

--- src/routes/Sports.js
+++ src/routes/Sports.js
@@ -2,25 +2,146 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import SportsNavbar from '../components/SportsNavbar';
 import "./Sports.css"
-import pic1 from '../images/blog2.png'
+import aitana from '../images/aitana.jpg'
+import xavi from '../images/xavi.jpg'
 
 const Sports = () => {
   return (
-    <div>
+    <div >
         <Navbar/>
         <SportsNavbar/>
+
+  <div className='heading'>
+    <h5 className='text1'>
+      Hot News!
+    </h5>
+  </div>
+
+ <div className='container'>
+  <div className='picture'>
+  <img src={xavi} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4 className='text2'>
+      OMG 😳🤯!! Xavi is about to be sacked from FC Barcelona
+      at the end of the season.
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4 className='text2'>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4 className='text2'>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4 className='text2'>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4 className='text2'>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h4>
+      hello Markss
+    </h4>
+  </div>
+</div>
+
+<div className='container'>
+  <div className='picture'>
+  <img src={aitana} alt='pic1' className='pic1' width='200px'/> 
+  </div>
+  <div className='sub-heading'>
+    <h1>
+      hello Markss
+    </h1>
+  </div>
+</div>
+
+
     </div>
   )
 }
 
-<div className='container'>
-  <div className='picture'>
-  <img src={pic1} alt='pic1' className='pic1' width='250px'/> 
-  </div>
-  <div className='heading'>
-    <h5>
-      hello Markss
-    </h5>
-  </div>
-</div>
 export default Sports;
